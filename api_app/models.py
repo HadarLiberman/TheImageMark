f9c898ce288273d43cfdaae0803610f3de00dd06
Hadar <libermanhadar@gmail.com>
2023-02-21 13:58:38 +0200

    migrate db

--- api_app/models.py
+++ api_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField("Name", max_length=240)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
